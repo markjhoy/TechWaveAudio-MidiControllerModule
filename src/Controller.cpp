1ad56925fd238129e525f82d1dad42081714dfe7
Mark Hoy <mark@markjhoy.com>
2026-07-28 15:08:22 -0400

    cleanups from last commit

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -52,14 +52,11 @@ void launch_midi_and_output_handler() {
         return;
     }
 
-    try {
-        tusb_rhport_init_t host_init = {
-            .role = TUSB_ROLE_DEVICE,
-            .speed = TUSB_SPEED_AUTO
-        };
-        tusb_init(BOARD_TUD_RHPORT, &host_init);
-    } catch (const std::exception& e) {
-    }
+    tusb_rhport_init_t host_init = {
+        .role = TUSB_ROLE_DEVICE,
+        .speed = TUSB_SPEED_AUTO
+    };
+    tusb_init(BOARD_TUD_RHPORT, &host_init);
 
     global_midi_output_handler->init();
 
